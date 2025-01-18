2c8f0b744f62c33bcf1e9a71fc417a71b2be0968
MAXASSASSINS <mohammedshadab23april@gmail.com>
2025-01-18 22:46:54 +0530

    Field Injection

--- src/main/java/com/example/DemoApp/Dev.java
+++ src/main/java/com/example/DemoApp/Dev.java
@@ -1,10 +1,15 @@
 package com.example.DemoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
 
+    @Autowired // Field Injection
+    Laptop laptop;
+
     public void build(){
+        laptop.compile();
         System.out.println("I am building a cool project :-)");
     }
 }
---
+++ src/main/java/com/example/DemoApp/Laptop.java
@@ -0,0 +1,11 @@
+package com.example.DemoApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile(){
+        System.out.println("Compiling through Laptop");
+    }
+}
